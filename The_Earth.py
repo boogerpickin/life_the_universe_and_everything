3a2cce624830f145a80472983d42fe04b803d200
CB Ladd <c.b.ladd@gmail.com>
2013-10-21 16:38:08 -0500

    The Begining.

---
+++ The_Earth.py
@@ -0,0 +1,5 @@
+class The_Earth:
+    def __init__(self):
+        self.form = ''
+        self.theDeep = []
+        self.theWaters = {}
---
+++ The_Heaven.py
@@ -0,0 +1,4 @@
+class The_Heaven:
+    def __init__(self):
+        self.data = []
+        
