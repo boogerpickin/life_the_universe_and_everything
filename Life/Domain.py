efec0021c6a33bf504606c35f1e730f3eabdc3d0
CB Ladd <c.b.ladd@gmail.com>
2013-10-22 12:42:59 -0500

    domainRandomize()

--- Life/Domain.py
+++ Life/Domain.py
@@ -1,13 +1,23 @@
 from Life import Life
+import random
 class Domain(Life):
-    def __init__(self, spark = 42):
+    def __init__(self, spark = 42, d = -1):
         Life.__init__(self)
+        self.d = d
         self.domains = ["Bacteria", "Archaea", "Eukaryota"]
+        def domainRandomize(self):
+                d = random.randint(0,2)
+                self.d = self.domains[d]
+                pass
+        if (d < 0):
+            domainRandomize(self)
+            pass
+
         if __name__ == '__main__':
             print "The Answer to Life, the Universe and Everything is: ", self.TheAnswer 
 
 newDomain = Domain(-1)
-print newDomain.TheAnswer
+print newDomain.d
         
 
 
--- Life/Life.py
+++ Life/Life.py
@@ -2,11 +2,12 @@ import random
 class Life(object):
     def __init__(self, spark = 42):
         self.TheAnswer = spark
+        def sparkRandomize(self):
+            self.TheAnswer = random.randint(40,45)
+            if (self.TheAnswer != 42):
+                print "The answer is not ", self.TheAnswer, ""
         while (self.TheAnswer != 42):
-            def sparkRandomize(self):
-                self.TheAnswer = random.randint(40,45)
-                if (self.TheAnswer != 42):
-                    print "The answer is not ", self.TheAnswer, ""
+            
             sparkRandomize(self)
         if __name__ == '__main__':
             print "The Answer to Life, the Universe and Everything is: ", self.TheAnswer 
